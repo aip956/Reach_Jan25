c4bd97e09e8bfa75095eeaee69fb596ab9338afb
Anthea Ip <antheaip964@gmail.com>
2024-12-28 17:51:38 -0500

    Adding leaderboard

--- play_LocalMM.sh
+++ play_LocalMM.sh
@@ -1,2 +1,16 @@
+# play_LocalMM.sh
+#!/bin/bash
+
+# Compile java files
+
 javac -cp "src:src/lib/*" src/DAO/*.java src/DBConnectionManager/*.java src/Models/*.java src/View/*.java src/MyMastermind.java
-java -cp "src:src/lib/*" MyMastermind
+
+
+# Check for leaderboard flag
+if [[ $1 == "--leaderboard" || $1 == "-l" ]]; then
+    # Pass leaderboard args to Java program
+    java -cp "src:src/lib/*" MyMastermind "$@"
+else
+    # Run w/o leaderboard
+    java -cp "src:src/lib/*" MyMastermind
+fi
