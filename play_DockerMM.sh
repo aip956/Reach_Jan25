e53a94fe7f5038b140496fe07f1a2f0b831637cc
Anthea Ip <antheaip964@gmail.com>
2025-01-03 19:48:18 -0500

    Added docker delay

--- play_DockerMM.sh
+++ play_DockerMM.sh
@@ -7,6 +7,9 @@ docker-compose down
 # Rebuild and restart the container
 docker-compose up --build  -d
 
+# Add delay so container is fully initialized
+sleep 2
+
 # Start the game
 docker exec -it game sh -c "java -cp '.:src/lib/*:src' MyMastermind --l 2"
 # docker-compose up -d && docker exec -it game sh -c "java -cp '.:src/lib/*:src' MyMastermind --l 2"
